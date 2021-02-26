bf6e4eaae08313ff331f373686567dc01a90cff3
rwxrob <rob@rwx.gg>
2021-02-26 08:47:59 -0500

    Fix example to match return params

--- example_test.go
+++ example_test.go
@@ -12,13 +12,11 @@ func ExamplePlain() {
 }
 
 func ExampleSecret() {
-	it, err := prompt.Secret("Enter secret (you won't see): ")
+	it := prompt.Secret("Enter secret (you won't see): ")
 	fmt.Println("Got:", it)
-	fmt.Println("Error:", err)
 }
 
 func ExampleStrict() {
-	it, err := prompt.Strict("Enter a number: ", "[0-9]")
+	it := prompt.Strict("Enter a number: ", "[0-9]")
 	fmt.Println("Got:", it)
-	fmt.Println("Error:", err)
 }
